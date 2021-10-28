fc3f9d6ad7dd0579e8fbc3da593f9dfa443ee644
Mark Kraay <63085173+markkraay@users.noreply.github.com>
2021-10-27 21:50:56 -0400

    Update DataStructures.py

--- October27/DataStructures.py
+++ October27/DataStructures.py
@@ -1,5 +1,5 @@
 # # Last week, we introduced to the basic concepts of datatypes through a
-# # simple in / output example
+# # simple input / output example
 
 # name = input()
 # print(type(name))
@@ -10,9 +10,9 @@
 # print(number + 1)
 
 
-# # These are These are great! But there's a lot more datatypes that are helpful
-# # I am ending these with a "_" because these words are RESERVED keyword in 
-# # Python. We can't use them as variable names
+# # These are great! But there's a lot more datatypes that are helpful
+# # I am ending these variable names with a "_" because these names are RESERVED keywords in 
+# # Python. Without the "_", we cannot use them as variable names
 # bool_ = True
 # print(type(bool_))
 
@@ -26,19 +26,19 @@
 # print(type(list_))
 
 # # Sets and maps both hold a collection of items. In sets however, there cannot be identitical items and 
-# # for this reason, the order does not matter
+# # for this reason, the order does not matter.
 
-# # To eludicate the concepts of lists and iteration, lets look at a how we can simplify a program using them
+# # To eludicate the concept of lists and iteration, lets look at a how we can simplify a program using them.
 
 # # Problem: Take 3 test scores as input and print the class average.
 
 # # Solution: 
-# # score1 = int(input())
-# # score2 = int(input())
-# # score3 = int(input())
-# # print((score1 + score2 + score3) / 3)
+# score1 = int(input())
+# score2 = int(input())
+# score3 = int(input())
+# print((score1 + score2 + score3) / 3)
 
-# # Not many classes only have three students, so how would we handle this if there was say 10 students.
+# # Not many classes only have three students, so how would we handle this if there was, say, 10 students.
 
 # # Solution
 
@@ -49,19 +49,19 @@
 # 	total += score
 # print(total / num_students)
 
-# What if we want to remember the test scores so that we can look at them later
+# # What if we want to remember the test scores so that we can look at them later
 
-## Introducing the LIST!
-# scores = [] # Introducing the array
+# # Introducing the LIST / ARRAY!
+# scores = [] # Creating the array
 # for i in range(num_students):
 # 	score = int(input("Enter your score: "))
 # 	scores.append(score)
 # print(scores)
 
-## Final senario. What if we want to identify a student with their test score
+# # Final senario. What if we want to identify a student with their test score
 
-## Introducing the DICTIOONARY!
-## A dictionary is collection of key: value pairs, so that we can use an items key to get its corresponding value
+# # Introducing the DICTIONARY!
+# # A dictionary is collection of key: value pairs. We can use an item's key to get its corresponding value
 
 # student_scores = {} # Creates a dictionary
 # for i in range(num_students):
@@ -71,12 +71,9 @@
 # print(student_scores)
 # print(student_scores["Mark"])
 
+# # Practice Problem:
+# # Given an array of integers your solution should find the smallest integer.
 
-
-# Practice Problem:
-# Given an array of integers your solution should find the smallest integer.
-
-# For example:
-# Given [34, 15, 88, 2] your solution will return 2
-# Given [34, -345, -1, 100] your solution will return -345
-# https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/python
+# # For example:
+# # Given [34, 15, 88, 2] your solution will return 2
+# # Given [34, -345, -1, 100] your solution will return -345
